7dde00594a1567770c4e96de6629519f0d1c7408
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-03-16 18:30:04 +0100

    chore: Made the env validation file a ts file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+import createJiti from "jiti"
+import {fileURLToPath} from "node:url";
+
+const jiti = createJiti(fileURLToPath((import.meta.url)))
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+jiti("~/env.ts");
 
 /** @type {import("next").NextConfig} */
 const config = {};
