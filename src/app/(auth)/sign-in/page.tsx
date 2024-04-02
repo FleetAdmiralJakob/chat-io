a060e7857d80903631d307173a03d875ba5c9158
Gamius00 <gescheaftlichgamius@gmx.de>
2024-04-02 13:50:12 +0200

    fix css

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 const SignInPage = () => {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center">
-      <div className="flex w-4/5 items-center justify-center gap-8 pb-16 pt-7  ">
+      <div className="flex w-4/5 items-center justify-center gap-8 pb-16">
         <Image src={icon} className={"w-20 xl:w-32"} alt="logo of Chat.io" />
-        <h1 className=" pr-4 text-xl font-bold tracking-tight sm:text-3xl">
+        <h1 className=" pr-9 text-xl font-bold tracking-tight sm:text-3xl">
           Welcome back to Chat.io
         </h1>
       </div>
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const SignInPage = () => {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center">
-      <div className="top-10 flex items-center justify-center gap-8 pb-12 pt-7 ">
+      <div className="flex w-4/5 items-center justify-center gap-8 pb-16">
         <Image src={icon} className={"w-20 xl:w-32"} alt="logo of Chat.io" />
         <h1 className=" pr-9 text-xl font-bold tracking-tight sm:text-3xl">
           Welcome to Chat.io
@@ -14,7 +14,7 @@ const SignInPage = () => {
       </div>
       <SignUpForm />
       <div className="bottom-3 left-0 flex w-full items-end justify-center xl:fixed">
-        <span className="flex w-4/5 items-center justify-center gap-8 pt-7">
+        <span className="flex w-2/3 items-center justify-center gap-8 pt-7">
           If you already have an account you can{" "}
           <Link href="/sign-in" className="underline">
             sign-in here
