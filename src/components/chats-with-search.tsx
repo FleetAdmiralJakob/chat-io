32ee7e8fb443c462cc9b57819ceff1030c694d5f
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-05-28 17:46:15 +0200

    feat: dev mode

--- src/components/chats-with-search.tsx
+++ src/components/chats-with-search.tsx
@@ -3,6 +3,9 @@
 import { AddUserDialog } from "~/components/add-user-dialog";
 import { SignOutButton, useUser } from "@clerk/nextjs";
 import Chats from "~/components/chat-overview";
+import { devMode$ } from "~/states";
+import { useState } from "react";
+import { DevMode } from "~/components/dev-mode-info";
 
 const ChatsWithSearch = ({
   classNameChatList,
@@ -10,18 +13,31 @@ const ChatsWithSearch = ({
   classNameChatList?: string;
 }) => {
   const { user } = useUser();
+  const [devModeClick, setDevModeClick] = useState<number>(0);
 
   return (
     <div className="min-w-96 pb-24 lg:ml-24">
       <div className="relative flex h-full w-full justify-center">
-        <h1 className="pt-28 text-4xl font-bold">Chats</h1>
+        <h1
+          className="pt-28 text-4xl font-bold"
+          onClick={() => {
+            setDevModeClick((prevState) => prevState + 1);
+
+            if (devModeClick >= 9) {
+              devMode$.set(true);
+            }
+          }}
+        >
+          Chats
+        </h1>
 
         <AddUserDialog classNameDialogTrigger="absolute bg-input right-16 top-16" />
       </div>
-      <p className="absolute top-0">
+      <DevMode className="lg:left-24">
         Current User: {user ? user.username : "No user"} <br />{" "}
+        <div onClick={() => devMode$.set(false)}>Disable dev mode</div>
         <SignOutButton />
-      </p>{" "}
+      </DevMode>{" "}
       <br />
       <Chats classNameChatList={classNameChatList} />
     </div>
---
+++ src/components/dev-mode-info.tsx
@@ -0,0 +1,23 @@
+import { observer } from "@legendapp/state/react";
+import { cn } from "~/lib/utils";
+import { devMode$ } from "~/states";
+
+export const DevMode = observer(
+  ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    return (
+      <div
+        className={cn("absolute left-0 top-0 hidden", className, {
+          block: devMode$.get(),
+        })}
+      >
+        {children}
+      </div>
+    );
+  },
+);
