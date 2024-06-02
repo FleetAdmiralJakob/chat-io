599e28a181b5d19ff81d00594db97ec14fd36839
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-06-02 15:58:11 +0200

    chore(not done): added barebones of frontend implementation of the notifications

--- src/sw.ts
+++ src/sw.ts
@@ -1,18 +1,54 @@
 import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry } from "@serwist/precaching";
-import { installSerwist } from "@serwist/sw";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
 
-declare const self: ServiceWorkerGlobalScope & {
-  // Change this attribute's name to your `injectionPoint`.
-  // `injectionPoint` is an InjectManifest option.
-  // See https://serwist.pages.dev/docs/build/inject-manifest/configuring
-  __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-};
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    // Change this attribute's name to your `injectionPoint`.
+    // `injectionPoint` is an InjectManifest option.
+    // See https://serwist.pages.dev/docs/build/configuring
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
 
-installSerwist({
+declare const self: ServiceWorkerGlobalScope;
+
+const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: [...defaultCache],
+  runtimeCaching: defaultCache,
 });
+
+self.addEventListener("push", (event) => {
+  const data = JSON.parse(event.data?.text() ?? "");
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.message,
+      icon: "/icons/icon-512x-512-any.png",
+    }),
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        if (clientList.length > 0) {
+          let client = clientList[0]!;
+          for (let i = 0; i < clientList.length; i++) {
+            if (clientList && clientList[i]!.focused) {
+              client = clientList[i]!;
+            }
+          }
+          return client.focus();
+        }
+        return self.clients.openWindow("/");
+      }),
+  );
+});
+
+serwist.addEventListeners();
