5899cd11d7b01bb56063ef3d4a294eedfa6e149e
Gamius00 <gescheaftlichgamius@gmx.de>
2024-06-07 13:01:33 +0200

    - h-full on Desktop Nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         { "hidden lg:flex": isChatPath },
       )}
     >
-      <div className="flex w-full justify-around lg:h-2/3 lg:flex-col">
+      <div className="flex h-full w-full justify-around lg:flex-col">
         <Link className={"flex flex-col items-center"} href={"/"}>
           <MessagesSquare
             className={cn({
