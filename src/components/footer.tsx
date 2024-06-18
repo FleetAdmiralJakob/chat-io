5a111e8b0117cdca8342e08b98ddcc2c31d45203
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-06-18 22:56:10 +0200

    fix: add some discernible text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -27,13 +27,22 @@ const Footer = () => {
             "flex w-full justify-around pt-7 text-3xl lg:ml-20 lg:mt-8 lg:w-1/6 lg:pt-0"
           }
         >
-          <Link href={"https://github.com/FleetAdmiralJakob/chat-io"}>
+          <Link
+            href={"https://github.com/FleetAdmiralJakob/chat-io"}
+            aria-label="See our project's code on GitHub"
+          >
             <FaGithub />
           </Link>
-          <Link href={"https://www.youtube.com/@JakobTheCoder"}>
+          <Link
+            href={"https://www.youtube.com/@JakobTheCoder"}
+            aria-label="The YouTube channel of one of the creators of Chat.io"
+          >
             <FaYoutube />
           </Link>
-          <Link href={"/contributors"}>
+          <Link
+            href={"/contributors"}
+            aria-label="See who worked on making this project happen"
+          >
             <FaUserGroup />
           </Link>
         </div>
@@ -45,7 +54,10 @@ const Footer = () => {
           <div className={"flex"}>
             <MdLanguage className={"mr-1 mt-0.5"} />{" "}
             <Select>
-              <SelectTrigger className="w-[180px]">
+              <SelectTrigger
+                className="w-[180px]"
+                aria-label="Trigger this to change the language of Chat.io"
+              >
                 <SelectValue placeholder="Languages" />
               </SelectTrigger>
               <SelectContent>
