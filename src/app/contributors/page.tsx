dcb6169e883e823d371df222eb6b02b8ec878ac5
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-06-19 19:44:22 +0200

    chore: improved code on contributor page

--- src/app/contributors/page.tsx
+++ src/app/contributors/page.tsx
@@ -2,40 +2,33 @@
 
 import Footer from "~/components/footer";
 import { IoIosArrowDropupCircle } from "react-icons/io";
-import JakobProfile from "/src/assets/jakob-profile.png";
-import FabiusProfile from "/src/assets/fabius-profile.png";
+import jakobProfile from "/src/assets/jakob-profile.png";
+import fabiusProfile from "/src/assets/fabius-profile.png";
 import { IoChevronBackSharp } from "react-icons/io5";
 import { FaLinkedin } from "react-icons/fa";
-import Image from "next/image";
+import Image, { type StaticImageData } from "next/image";
 import { cn } from "~/lib/utils";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FaLink } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { BsPeopleFill } from "react-icons/bs";
+import Link from "next/link";
 
 interface ContributorsProps {
   name: string;
   job: string;
-  image: JSX.Element;
+  image: StaticImageData;
   github?: string;
   website?: string;
   linkedin?: string;
 }
 
-const ContributorList: ContributorsProps[] = [
+const contributorList: ContributorsProps[] = [
   {
     name: "Fabius Schurig",
     job: "Engineer & Designer",
-    image: (
-      <Image
-        className="rounded-full"
-        alt={"Jakob"}
-        width={50}
-        height={50}
-        src={FabiusProfile}
-      />
-    ),
+    image: fabiusProfile,
     github: "https://github.com/Gamius00",
     website: "https://schurig.tech",
     linkedin: "https://www.linkedin.com/in/fabius-schurig-80713b284/",
@@ -43,15 +36,7 @@ const ContributorList: ContributorsProps[] = [
   {
     name: "Jakob Rössner",
     job: "Engineer",
-    image: (
-      <Image
-        className="rounded-full"
-        alt={"Jakob"}
-        width={50}
-        height={50}
-        src={JakobProfile}
-      />
-    ),
+    image: jakobProfile,
     github: "https://github.com/FleetAdmiralJakob",
     website: "https://roessner.tech",
     linkedin: "https://www.linkedin.com/in/jakobroessner/",
@@ -77,7 +62,7 @@ const Contributors = () => {
           </div>
         </div>
         <p className="mb-20 text-xl">Made with ❤️ by </p>
-        {ContributorList.map((contributor, id) => {
+        {contributorList.map((contributor, id) => {
           const [cardOpen, setCardOpen] = useState(false);
           return (
             <>
@@ -88,7 +73,13 @@ const Contributors = () => {
               >
                 <div className="flex lg:justify-between" key={id}>
                   <div className="flex flex-wrap items-start justify-start">
-                    <p className="order-0 lg:flex">{contributor.image}</p>
+                    <Image
+                      src={contributor.image}
+                      alt={contributor.name}
+                      width={50}
+                      height={50}
+                      className="order-0 rounded-full lg:flex"
+                    />
 
                     <div className="order-1 ml-4 mt-0.5 lg:flex lg:justify-between">
                       <p className="text-xl font-bold lg:mt-3">
@@ -120,41 +111,36 @@ const Contributors = () => {
                     },
                   )}
                 >
-                  <div
-                    className={cn("hidden", {
-                      flex: contributor.website,
-                    })}
-                    onClick={() => {
-                      if (contributor.website) {
-                        router.push(contributor.website);
-                      }
-                    }}
-                  >
-                    <p className={"hidden cursor-pointer lg:flex"}>Website</p>
-                    <FaLink className="ml-1 mt-1" />
-                  </div>
-                  <div
-                    className={cn("hidden", { flex: contributor.linkedin })}
-                    onClick={() => {
-                      if (contributor.linkedin) {
-                        router.push(contributor.linkedin);
-                      }
-                    }}
-                  >
-                    <p className={"hidden cursor-pointer lg:flex"}>LinkedIn</p>
-                    <FaLinkedin className="ml-1 mt-2.5 lg:mt-1" />
-                  </div>
-                  <div
-                    className={cn("hidden", { flex: contributor.github })}
-                    onClick={() => {
-                      if (contributor.github) {
-                        router.push(contributor.github);
-                      }
-                    }}
-                  >
-                    <p className={"hidden cursor-pointer lg:flex"}>GitHub</p>
-                    <FaGithub className="ml-1 mt-2.5 lg:mt-1" />
-                  </div>
+                  {contributor.website ? (
+                    <Link
+                      className={cn("hidden", {
+                        flex: contributor.website,
+                      })}
+                      href={contributor.website}
+                    >
+                      <p className={"hidden cursor-pointer lg:flex"}>Website</p>
+                      <FaLink className="ml-1 mt-1" />
+                    </Link>
+                  ) : null}
+
+                  {contributor.linkedin ? (
+                    <Link className="flex" href={contributor.linkedin}>
+                      <p className={"hidden cursor-pointer lg:flex"}>
+                        LinkedIn
+                      </p>
+                      <FaLinkedin className="ml-1 mt-2.5 lg:mt-1" />
+                    </Link>
+                  ) : null}
+
+                  {contributor.github ? (
+                    <Link
+                      className={cn("hidden", { flex: contributor.github })}
+                      href={contributor.github}
+                    >
+                      <p className={"hidden cursor-pointer lg:flex"}>GitHub</p>
+                      <FaGithub className="ml-1 mt-2.5 lg:mt-1" />
+                    </Link>
+                  ) : null}
                 </div>
               </div>
             </>
