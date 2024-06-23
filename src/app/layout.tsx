fbee3ab82f255e58577b59917dc1636cfd96c78e
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-06-23 09:16:29 +0200

    chore: moved theme-provider to the other providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "~/styles/globals.css";
-import { ThemeProvider } from "~/components/theme-provider";
+import { ThemeProvider } from "~/components/providers/theme-provider";
 import type { Metadata, Viewport } from "next";
 import { cn } from "~/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
