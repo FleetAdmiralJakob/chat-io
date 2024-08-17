2eb6c86047deec6c54c3fb65ff8f5ef581bd5a4b
Jakob Rössner <jakob@lylu.de>
2024-08-17 23:40:48 +1000

    style: updated homepage and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { FaGithub, FaYoutube, FaUserGroup, FaBook } from "react-icons/fa6";
-import { MdLanguage } from "react-icons/md";
 import {
   Select,
   SelectContent,
@@ -13,6 +11,8 @@ import {
 import Image from "next/image";
 import england from "src/assets/united-kingdom.png";
 import germany from "src/assets/germany.png";
+import { SiGithub, SiYoutube } from "react-icons/si";
+import { Book, Globe, UsersRound } from "lucide-react";
 
 const Footer = () => {
   return (
@@ -31,31 +31,27 @@ const Footer = () => {
             href={"https://github.com/FleetAdmiralJakob/chat-io"}
             aria-label="See our project's code on GitHub"
           >
-            <FaGithub />
+            <SiGithub />
           </Link>
           <Link
             href={"https://www.youtube.com/@JakobTheCoder"}
             aria-label="The YouTube channel of one of the creators of Chat.io"
           >
-            <FaYoutube />
+            <SiYoutube />
           </Link>
           <Link
             href={"/contributors"}
             aria-label="See who worked on making this project happen"
           >
-            <FaUserGroup />
+            <UsersRound className="h-7 w-7" />
           </Link>
         </div>
-        <div
-          className={
-            "mt-8 block w-full pl-14 lg:flex lg:w-2/5 lg:justify-around"
-          }
-        >
-          <div className={"flex"}>
-            <MdLanguage className={"mr-1 mt-0.5"} />{" "}
+        <div className="flex w-full flex-col gap-3 pl-14 pt-8 lg:w-2/5 lg:flex-row lg:justify-around lg:pt-0">
+          <div className={"flex items-center gap-1"}>
+            <Globe />{" "}
             <Select>
               <SelectTrigger
-                className="w-[180px]"
+                className="flex w-[180px] items-center py-0"
                 aria-label="Trigger this to change the language of Chat.io"
               >
                 <SelectValue placeholder="Languages" />
@@ -91,8 +87,8 @@ const Footer = () => {
               </SelectContent>
             </Select>
           </div>
-          <Link href={"/legal"} className={"flex"}>
-            <FaBook className={"mb-5 mr-1 mt-1"} /> Legal
+          <Link href={"/legal"} className={"flex items-center gap-1"}>
+            <Book /> Legal
           </Link>
         </div>
       </div>
