717cd250108bceafddccf0b9b7b263a4e5eca003
Jakob Rössner <jakob.roessner@outlook.de>
2024-11-02 19:48:07 +1100

    fix(styles): auth pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,20 +5,18 @@ import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <main>
-      <div className="relative flex min-h-screen flex-col items-center justify-center">
-        <div className="flex w-4/5 items-center justify-center gap-8 pb-16">
-          <Image src={icon} className={"w-20 xl:w-24"} alt="logo of Chat.io" />
-          <h1 className="pr-9 text-xl font-bold tracking-tight sm:text-3xl">
-            Welcome back to Chat.io
-          </h1>
-        </div>
-        <SignInForm />
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <div className="mt-10 flex w-4/5 items-center justify-center gap-8 pb-4 xl:mt-0">
+        <Image src={icon} className={"w-20 xl:w-24"} alt="logo of Chat.io" />
+        <h1 className="pr-9 text-xl font-bold tracking-tight sm:text-3xl">
+          Welcome back to Chat.io
+        </h1>
       </div>
-      <div className="bottom-3 left-0 flex w-full items-end justify-center xl:fixed">
-        <span className="mb-7 flex w-3/4 items-center justify-center gap-8">
+      <SignInForm />
+      <div className="bottom-3 left-0 flex w-full items-end justify-center pb-7 text-center">
+        <span className="flex w-3/4 items-center justify-center gap-8 pt-7">
           <div>
-            If you not have an account you can{" "}
+            If you don&#39;t have an account you can{" "}
             <Link href="/sign-up" className="underline">
               sign-up here
             </Link>
