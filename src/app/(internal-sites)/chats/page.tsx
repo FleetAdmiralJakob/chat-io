58d0bb330fff040644ad821e19e2892bbf11e499
FleetAdmiralJakob <jakob.roessner@outlook.de>
2026-01-05 21:12:14 +0100

    format

--- src/app/(internal-sites)/chats/page.tsx
+++ src/app/(internal-sites)/chats/page.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import ChatWithSearch from "~/components/chats-with-search";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Chats",
