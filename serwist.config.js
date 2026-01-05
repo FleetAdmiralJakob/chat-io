55db41e147ea95abe39d6142619b05fac14ff404
FleetAdmiralJakob <jakob.roessner@outlook.de>
2026-01-05 19:17:05 +0100

    Commented out not needed code

--- serwist.config.js
+++ serwist.config.js
@@ -1,16 +1,18 @@
 // @ts-check
+// import { spawnSync } from "node:child_process";
 // If you want to use the fully resolved Next.js
 // configuration to resolve Serwist configuration,
 // use `serwist.withNextConfig` instead.
-import { spawnSync } from "node:child_process";
 import { serwist } from "@serwist/next/config";
 
 // Using `git rev-parse HEAD` might not the most efficient
 // way of determining a revision. You may prefer to use
 // the hashes of every extra file you precache.
+/*
 const revision =
   spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout ??
   crypto.randomUUID();
+ */
 
 export default serwist.withNextConfig((nextConfig) => ({
   swSrc: "src/sw.ts",
